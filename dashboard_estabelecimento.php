98c3fe55fb3d19e835254a6dcc3528b1ab1d2564
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-06-22 21:26:54 -0300

    Update dashboard_estabelecimento.php

--- dashboard_estabelecimento.php
+++ dashboard_estabelecimento.php
@@ -1,51 +1,64 @@
 <?php
+session_start();
 require_once '../conexao.php';
 
-session_start();
-
-if (!isset($_SESSION['estabelecimento_id'])) {
-    header("Location: login_estabelecimento.php");
+function gerarCodigo()
+{
+    $caracteres = '0123456789abcdefghijklmnopqrstuvwxyz';
+    $codigo = '';
+    for ($i = 0; $i < 6; $i++) {
+        $codigo .= $caracteres[rand(0, strlen($caracteres) - 1)];
+    }
+    return $codigo;
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $codigo = bin2hex(random_bytes(5)); // Gera um código único
-
-    try {
-        $query = $conn->prepare("INSERT INTO Pontos (id_estabelecimento, codigo, pontos) VALUES (?, ?, 0)");
-        $query->execute([$_SESSION['estabelecimento_id'], $codigo]);
-        $msg = "Código gerado com sucesso: " . $codigo;
-    } catch (PDOException $e) {
-        $msg = "Erro ao gerar o código: " . $e->getMessage();
+function codigoUnico(PDO $conn)
+{
+    while (true) {
+        $codigo = gerarCodigo();
+        $query = $conn->prepare("SELECT * FROM Pontos WHERE codigo = ?");
+        $query->execute([$codigo]);
+        if ($query->rowCount() == 0) {
+            return strtoupper($codigo);
+        }
     }
 }
 
-try {
-    $query = $conn->prepare("SELECT * FROM Pontos WHERE id_estabelecimento = ?");
-    $query->execute([$_SESSION['estabelecimento_id']]);
-    $codigos = $query->fetchAll(PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    $msg = "Erro ao recuperar os códigos: " . $e->getMessage();
+// Gerando o código
+if (isset($_POST['gerar_codigo'])) {
+    $codigo = codigoUnico($conn);
+    $query = $conn->prepare("INSERT INTO Pontos (id_estabelecimento, codigo, pontos) VALUES (?, ?, 0)");
+    $query->execute([$_SESSION['estabelecimento_id'], $codigo]);
 }
+
+// Buscando os códigos
+$query = $conn->prepare("SELECT * FROM Pontos WHERE id_estabelecimento = ?");
+$query->execute([$_SESSION['estabelecimento_id']]);
+$codigos = $query->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="dashboard_estabelecimento.css">
     <title>Dashboard do Estabelecimento</title>
+    <link rel="stylesheet" href="dashboard_estabelecimento.css">
 </head>
+
 <body>
-    <div class="container">
-        <h1>Bem-vindo, <?php echo $_SESSION['estabelecimento_nome']; ?>!</h1>
-        <form action="" method="post">
-            <input type="submit" value="Gerar Código">
-        </form>
-        <?php if(isset($msg)) echo $msg; ?>
-        <h2>Seus Códigos:</h2>
-        <ul>
-            <?php foreach($codigos as $codigo) echo "<li>".$codigo['codigo']."</li>"; ?>
-        </ul>
-    </div>
+    <h1>Dashboard do Estabelecimento</h1>
+    <form action="dashboard_estabelecimento.php" method="post">
+        <button type="submit" name="gerar_codigo">Gerar Código</button>
+    </form>
+    <h2>Códigos gerados:</h2>
+    <ul>
+        <?php foreach ($codigos as $codigo): ?>
+            <li>
+                <?php echo $codigo['codigo']; ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </body>
+
 </html>
