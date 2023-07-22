d13a11ff3ade9f302f1e2c970ea091bc2f2cb853
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-07-22 02:16:05 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,39 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Adicione esta linha -->
     <title>Cadastro de Cliente</title>
-    <link rel="stylesheet" href="cadastro_style.css">
+    <link rel="stylesheet" href="perfil_style.css">
 </head>
 
 <body>
-    <h1>Cadastre-se aqui!</h1>
-    <form action="cadastrar_cliente.php" method="post">
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome" required>
+    <div class="header">
+        <h1>Cadastre-se aqui!</h1>
+        <form action="cadastrar_cliente.php" method="post">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" required>
 
-        <label for="sobrenome">Sobrenome:</label>
-        <input type="text" id="sobrenome" name="sobrenome" required>
+            <label for="sobrenome">Sobrenome:</label>
+            <input type="text" id="sobrenome" name="sobrenome" required>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
 
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required>
+            <label for="senha">Senha:</label>
+            <input type="password" id="senha" name="senha" required>
 
-        <label for="confirmar_senha">Confirmar Senha:</label>
-        <input type="password" id="confirmar_senha" name="confirmar_senha" required>
+            <label for="confirmar_senha">Confirmar Senha:</label>
+            <input type="password" id="confirmar_senha" name="confirmar_senha" required>
 
-        <!-- Adicionando campo Data de Nascimento -->
-        <label for="data_nascimento">Data de Nascimento:</label>
-        <input type="date" id="data_nascimento" name="data_nascimento" required>
+            <!-- Adicionando campo Data de Nascimento -->
+            <label for="data_nascimento">Data de Nascimento:</label>
+            <input type="date" id="data_nascimento" name="data_nascimento" required>
 
-        <!-- Adicionando campo Curso -->
-        <label for="curso">Curso:</label>
-        <select id="curso" name="curso" required>
-            <option value="">Selecione o curso</option>
-            <?php
+            <!-- Adicionando campo Curso -->
+            <label for="curso">Curso:</label>
+            <select id="curso" name="curso" required>
+                <option value="">Selecione o curso</option>
+                <?php
                 include '../conexao.php';
 
                 $query = "SELECT * FROM cursos";
@@ -42,15 +44,17 @@
                 while ($row = mysqli_fetch_assoc($result)) {
                     echo '<option value="' . $row['id'] . '">' . $row['nome_curso'] . '</option>';
                 }
-            ?>
-        </select>
+                ?>
+            </select>
 
-        <label for="aceitar_politica_privacidade">
-            <input type="checkbox" id="aceitar_politica_privacidade" name="aceitar_politica_privacidade" required>
-            Eu concordo com a <a href="politica_privacidade.php" target="_blank">Política de Privacidade</a>.
-        </label>
+            <label for="aceitar_politica_privacidade">
+                <input type="checkbox" id="aceitar_politica_privacidade" name="aceitar_politica_privacidade" required>
+                Eu concordo com a <a href="politica_privacidade.php" target="_blank">Política de Privacidade</a>.
+            </label>
 
-        <input type="submit" value="Cadastrar">
-    </form>
+            <input type="submit" value="Cadastrar">
+        </form>
+    </div>
 </body>
+
 </html>
