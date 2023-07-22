30ddb0d3fa17a9ec3ea5b9e4ce7a86276b45d1e3
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-07-22 02:07:26 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9257998236665235"
+        crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cartão Fidelidade Digital</title>
+    <link rel="stylesheet" href="landing_page_style.css">
+</head>
+
+<body>
+    <header>
+        <h1>UltraCheck</h1>
+        <nav>
+            <a href="politica_privacidade.php">Política de Privacidade</a>
+            <a href="#contact">Contato</a>
+        </nav>
+    </header>
+    <main>
+        <section class="intro">
+            <h2>Tchauzinho Cartões Físicos!</h2>
+            <p>
+                É hora de experimentar uma nova maneira de ganhar pontos ao comprar qualquer bebida na UltraCoffe!
+            </p>
+            <div class="buttons">
+                <a href="login.php" class="button">Acessar</a>
+                <a href="cadastro.php" class="button">Cadastrar</a>
+            </div>
+        </section>
+
+        <section class="benefits">
+            <h2>Benefícios do Sistema</h2>
+            <div class="benefit-block">
+                <h3>É digital!</h3>
+                <p>Chega de juntar vários cartões físicos, com o UltraCheck você tem todos eles na palma da sua mão a
+                    hora que quiser!</p>
+            </div>
+            <!-- Outros blocos de benefícios podem ser adicionados aqui -->
+
+            <div class="benefit-block">
+                <h3>Acompanhe seu progresso!</h3>
+                <p>Nosso aplicativo fácil de usar permite que você monitore seu progresso e veja o quanto falta para
+                    alcançar seu próximo prêmio. Fique de olho em seu progresso e se motive para ganhar mais!</p>
+            </div>
+
+            <div class="benefit-block">
+                <h3>Promoções exclusivas!</h3>
+                <p>Como membro do UltraCheck, você terá acesso a ofertas e promoções exclusivas, criadas especialmente
+                    para nossos clientes mais fiéis!</p>
+            </div>
+
+        </section>
+
+        <section class="about">
+            <h2>O que é o sistema</h2>
+            <p>
+                O UltraCheck é uma plataforma de cartão fidelidade digital que permite que você acompanhe seus pontos,
+                prêmios e muito mais, tudo pelo seu smartphone. Com o UltraCheck, você nunca mais terá que se preocupar
+                em perder seus cartões de papel ou esquecer de levá-los com você.
+            </p>
+        </section>
+
+        <section class="how-it-works">
+            <h2>Como Funciona</h2>
+            <ol>
+                <li>
+                    <h3>Cadastre-se</h3>
+                    <p>Crie sua conta no UltraCheck e tenha acesso a todos os benefícios oferecidos pela plataforma.</p>
+                </li>
+                <li>
+                    <h3>Compre e acumule pontos</h3>
+                    <p>A cada compra realizada, você acumula pontos que podem ser trocados por prêmios e descontos
+                        especiais.</p>
+                </li>
+                <li>
+                    <h3>Acompanhe seu progresso</h3>
+                    <p>Monitore seu saldo de pontos e veja o quanto falta para atingir o próximo prêmio.</p>
+                </li>
+                <li>
+                    <h3>Resgate prêmios</h3>
+                    <p>Quando atingir a quantidade necessária de pontos, troque-os por prêmios e descontos exclusivos.
+                    </p>
+                </li>
+            </ol>
+        </section>
+
+        <section class="faq">
+            <h2>Perguntas Frequentes</h2>
+            <div class="benefit-block">
+                <h3>Como faço para me cadastrar?</h3>
+                <p>Para se cadastrar, clique no botão "Cadastrar" na página inicial e preencha o formulário com suas
+                    informações.</p>
+            </div>
+            <div class="benefit-block">
+                <h3>Como acumulo pontos?</h3>
+                <p>Acumule pontos realizando compras nos estabelecimentos parceiros. Os pontos serão adicionados
+                    automaticamente à sua conta UltraCheck.</p>
+            </div>
+            <div class="benefit-block">
+                <h3>Como resgato prêmios?</h3>
+                <p>Quando acumular pontos suficientes, você poderá trocá-los por prêmios e descontos especiais
+                    disponíveis na plataforma.</p>
+            </div>
+            <!-- Outras perguntas frequentes podem ser adicionadas aqui -->
+        </section>
+
+        <section class="contact" id="contact">
+            <h2>Entre em contato</h2>
+            <p>
+                Quer saber mais sobre o UltraCheck? Entre em contato conosco pelo e-mail: <a
+                    href="mailto:contato@ultrafidelidade.com">contato@ultrafidelidade.com</a>
+            </p>
+        </section>
+
+    </main>
+</body>
+
+</html>
