dc2ca2a65e9b4b1a905511578afce01af6ee3c6c
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-07-29 11:21:05 -0300

    Update perfil_cafeteria.php

--- perfil_cafeteria.php
+++ perfil_cafeteria.php
@@ -16,6 +16,10 @@ verify_session('email_cafeteria', 'login_cafeteria.php');
 require 'vendor/autoload.php';
 
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 
 function exportData($data)
