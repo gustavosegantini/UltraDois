9de59a5c6c29ef8525fd1f0c6ed43c764f6179b3
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-08-25 16:12:15 -0300

    Update login_cafeteria.php

--- login_cafeteria.php
+++ login_cafeteria.php
@@ -1,15 +1,22 @@
-<?php session_start(); // Adicione esta linha para iniciar a sessão ?>
-
+<?php session_start(); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login da Cafeteria</title>
     <link rel="stylesheet" href="login_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
+
 <body>
-    <h1>Login da Cafeteria</h1>
-    <?php
+    <div class="container">
+        <div class="modal">
+            <header>
+                <h1>Login da Cafeteria</h1>
+            </header>
+            <?php
             if (isset($_SESSION['loginErro'])) {
                 echo '<div class="alert alert-danger">' . $_SESSION['loginErro'] . '</div>';
                 unset($_SESSION['loginErro']);
@@ -24,18 +31,46 @@
                 echo '<div class="alert alert-danger">' . $_SESSION['loginInvalido'] . '</div>';
                 unset($_SESSION['loginInvalido']);
             }
-        ?>
-    <div class="container">
-        <form action="autenticar_login_cafeteria.php" method="post">
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
+            ?>
 
-            <label for="senha">Senha:</label>
-            <input type="password" id="senha" name="senha" required>
+            <form method="POST" action="autenticar_login_cafeteria.php">
+                <div class="input-wrapper">
+                    <label for="email">E-mail:</label>
+                    <input type="email" id="email" name="email" placeholder="exemple@mail.com" required>
+                </div>
 
-            <input type="submit" value="Entrar">
-        </form><br>
-        <p>Ainda não possui uma conta? <a href="cadastro_cafeteria.php">Cadastre-se</a></p>
-    <div>
+                <div class="input-wrapper">
+                    <label for="senha">Senha:</label>
+                    <input type="password" id="senha" name="senha" placeholder="Sua Senha" required>
+                    <i class="far fa-eye password-icon" onclick="togglePasswordVisibility('senha')"></i>
+                </div>
+
+                <input type="submit" value="Entrar">
+            </form><br>
+            <p>Ainda não possui uma conta? <a href="cadastro_cafeteria.php">Cadastre-se</a></p>
+        </div>
+    </div>
+    <footer>
+        <div class="credit-banner">
+            <p>Criado por Gustavo Segantini</p>
+        </div>
+    </footer>
 </body>
+<script>
+    function togglePasswordVisibility(inputId) {
+        const input = document.getElementById(inputId);
+        const icon = input.nextElementSibling;
+
+        if (input.type === 'password') {
+            input.type = 'text';
+            icon.classList.remove('fa-eye');
+            icon.classList.add('fa-eye-slash');
+        } else {
+            input.type = 'password';
+            icon.classList.remove('fa-eye-slash');
+            icon.classList.add('fa-eye');
+        }
+    }
+</script>
+
 </html>
