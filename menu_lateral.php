8d061e1645dae0c2512f7d15e525042b237df872
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-09-14 11:05:57 -0300

    Create menu_lateral.php

---
+++ menu_lateral.php
@@ -0,0 +1,7 @@
+<div id="menu-lateral">
+    <ul>
+        <li><a href="perfil_cafeteria.php?page=resumo">Resumo</a></li>
+        <li><a href="perfil_cafeteria.php?page=produtos">Produtos</a></li>
+        <!-- Adicione mais links para novas funcionalidades aqui -->
+    </ul>
+</div>
