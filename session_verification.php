8ece3e2f379454ca1f14cf7f95a665bafae06f3f
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-11-19 10:53:15 -0300

    a

--- session_verification.php
+++
@@ -1,8 +0,0 @@
-<?php
-function verify_session($session_key, $redirect_page) {
-    if (!isset($_SESSION[$session_key])) {
-        header("Location: $redirect_page");
-        exit();
-    }
-}
-?>
